5c398b42ac3a9253b9aa02e295efd910867e781c
Vinay <vinay061@gmail.com>
2021-05-04 18:27:16 +0200

    Header component

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Header = ({ title }) => {
+    return (
+        <div>
+            <h1>{title}</h1>
+        </div>
+    )
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired
+}
+export default Header
